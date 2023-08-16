7ee490a694b6bfd8e99f85bdd292506009d4ac8e
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-16 13:55:39 -0400

    Add base level imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-
+import { useState } from "react";
+import data from "./data/data.json";
 function App() {
+  for (let student of data) {
+    console.log(student);
+  }
   return (
     <div>
       <h1>Student Dashboard</h1>
